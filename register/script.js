cd9d8619e4992a4ec0e9fbfaa4a46687db06ac8e
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-12-14 22:35:45 +0530

    Add landing registration page

--- register/script.js
+++ register/script.js
@@ -5,19 +5,19 @@ $(document).ready(function()  {
   
   $("#left").mouseover(function() {
     $(".logo, .name").css("color", "white");
-    $(".logo1, .name1").css("color", "black");
-    $("#left").css("background-color", "black");
+    $(".logo1, .name1").css("color", "#0f1216");
+    $("#left").css("background-color", "#0f1216");
     $("#right").css("background-color", "white");
   });
   $("#right").mouseover(function() {
     $(".logo1, .name1").css("color", "white");
-    $(".logo, .name").css("color", "black");
-    $("#right").css("background-color", "black");
+    $(".logo, .name").css("color", "#0f1216");
+    $("#right").css("background-color", "#0f1216");
     $("#left").css("background-color", "white");
   });
   $("#left, #right").mouseleave(function() {
-    $(".logo, .name").css("color", "black");
-    $(".logo1, .name1").css("color", "black");
+    $(".logo, .name").css("color", "#0f1216");
+    $(".logo1, .name1").css("color", "#0f1216");
     $("#right").css("background-color", "white");
     $("#left").css("background-color", "white");
   });
@@ -27,31 +27,31 @@ $(document).ready(function()  {
     if($("#right").hasClass("col-md-6")){
         $(this).toggleClass("col-md-6 col-md-12",1000);
     }
-    $("#right").hide(700);
+    $("#right").hide(1000);
     $(".logo").hide();
     $(".name").addClass("formopen");
-    $(".popup1").fadeIn(3000);
+    $(".popup1").fadeIn(1000);
     $("#left").mouseover(function() {
       $("#left").css("background-color", "white");
-      $(".name").css("color", "black");
+      $(".name").css("color", "#0f1216");
     });
     $("#exit").fadeIn();
   });
 
   $("#exit").click(function() {
     if($("#left").hasClass("col-md-12")){
-      $("#left").toggleClass("col-md-12 col-md-6",1000);
+      $("#left").toggleClass("col-md-12 col-md-6",2000);
     }
     $(".name").removeClass("formopen");
-    $("#right").show(700);
+    $("#right").show(1000);
     $(".logo").show();
     $(".logo, .name").css("text-align", "center");
     $(".name").css("padding-top", "15%");
     $(".popup1").hide();
     $("#left").mouseover(function() {
       $(".logo, .name").css("color", "white");
-      $(".logo1, .name1").css("color", "black");
-      $("#left").css("background-color", "black");
+      $(".logo1, .name1").css("color", "#0f1216");
+      $("#left").css("background-color", "#0f1216");
       $("#right").css("background-color", "white");
     });  
     $("#exit").fadeOut(); 
@@ -62,31 +62,31 @@ $(document).ready(function()  {
     if($("#right").hasClass("col-md-6")){
         $(this).toggleClass("col-md-6 col-md-12",1000);
     }
-    $("#left").hide(700);
+    $("#left").hide();
     $(".logo1").hide();
     $(".name1").addClass("formopen");
-    $(".popup2").fadeIn(3000);
+    $(".popup2").fadeIn(1000);
     $("#right").mouseover(function() {
       $("#right").css("background-color", "white");
-      $(".name1").css("color", "black");
+      $(".name1").css("color", "#0f1216");
     });
     $("#exit").fadeIn();
   });
 
   $("#exit").click(function() {
     if($("#right").hasClass("col-md-12")){
-      $("#right").toggleClass("col-md-12 col-md-6",1000);
+      $("#right").toggleClass("col-md-12 col-md-6",2000);
     }
     $(".name1").removeClass("formopen");
-    $("#left").show(700);
+    $("#left").show(2000);
     $(".logo1").show();
     $(".logo1, .name1").css("text-align", "center");
     $(".name1").css("padding-top", "15%");
     $(".popup2").hide();
     $("#right").mouseover(function() {
       $(".logo1, .name1").css("color", "white");
-      $(".logo, .name").css("color", "black");
-      $("#right").css("background-color", "black");
+      $(".logo, .name").css("color", "#0f1216");
+      $("#right").css("background-color", "#0f1216");
       $("#left").css("background-color", "white");
     });
     $("#exit").fadeOut(); 
