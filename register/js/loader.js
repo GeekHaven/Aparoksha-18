16fa04103f2a05cfd3a81c0200787bac17225cb1
Ananya Agrawal <33188930+ananya-agrawal@users.noreply.github.com>
2018-01-15 20:02:16 +0530

    Create loader.js

---
+++ register/js/loader.js
@@ -0,0 +1,16 @@
+$(document).ready(function($) {
+    // make loader of full size here.
+    var Body = $('body');
+    Body.addClass('loader');
+});
+$(window).on('load', function () {
+    $('.tetris').fadeOut();
+    //make site visible again
+    $('body').removeClass('loader');
+});
+
+
+
+
+
+
