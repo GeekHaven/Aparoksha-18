221abea85b6b28933502555063f2a96bc29a0f3a
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-03-02 21:58:52 +0530

    Add check status page for tshirt

--- tshirt/order/check.php
+++ tshirt/order/check.php
@@ -1,5 +1,63 @@
 <?php
-session_start();
+
+  session_start();
+  require '../../register/vendor/autoload.php';
+  $dotenv = new Dotenv\Dotenv(__DIR__);
+    if (file_exists('.env')) {
+       $dotenv->load('.env');
+    }
+
+    $dbhost = getenv('DB_HOST');
+    $dbuser = getenv('DB_USER');
+    $dbpass = getenv('DB_PASS');
+    $dbn = getenv('DB_NAME');
+    $tbn = getenv('TB_NAME');
+
+    $servername = $dbhost;
+    $username = $dbuser;
+    $password = $dbpass;
+    $dbname = $dbn;
+    $tbname = $tbn;
+
+    if(isset($_GET['orderid']))
+    {
+      $orderno = $_GET['orderid'];
+
+      try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->prepare("SELECT * FROM $tbname WHERE order_no = :order_no");
+        $stmt->execute(['order_no' => $orderno]);
+        $order = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($order != null){
+            $_SESSION['confirm'] = "Your order details are shown below"; 
+            $verified = $order['verified'];
+            $name = $order['name'];
+            $ttype = $order['t_type'];
+            $tsize = $order['t_size'];
+            $quantity = $order['t_quantity'];
+            $tranid = $order['trans_id'];
+            $amount = $order['amount_paid'];
+        }
+              
+        else{
+            $_SESSION['confirm'] = "There is no order with this order ID. Check order ID with the email sent to you regarding your order ";
+        }
+            
+      }
+    
+      catch(PDOException $e){
+        $_SESSION['confirm'] = "Oops! looks like we have ran into some trouble with your order. Please
+        try again after some time. If problem persists please feel free to contact any person mentioned below ";
+        header("Refresh: 0; url=index.php#info");
+        exit;
+      }
+      $clicked = false;
+      $conn = null;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +68,7 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Aparoksha 2018 registrations are open. Dates are 16th- 18th March 2018. Exciting events">
   <meta name="author" content="Aproksha 2018">
-  <title>Aparoksha Registrations: Check Your Status</title>
+  <title>Aparoksha Merchandise| Check Your Status</title>
 
   <!-- Bootstrap -->
   <link href="../../css/bootstrap.min.css" rel="stylesheet">
@@ -37,82 +95,78 @@ session_start();
         </div>
       </div>
 
-      <div class="text-center">
-       <h5>Check back later. In progress </h5>
-      </div>
-      <!-- <div class="container">
+      <div class="container">
         <div class="row me-row content-ct">
-          <div class="col-md-4" style="margin-top:1em;">
-            <a href="index.php" class="btn btn-lg btn-red">Register<i class="fa fa-calendar" aria-hidden="true" style="padding-left:0.6em;"></i>
+          <div class="col-md-6" style="margin-top:1em;">
+            <a href="index.php" class="btn btn-lg btn-red">Order<i class="fa fa-calendar" aria-hidden="true" style="padding-left:0.6em;"></i>
             </a>
           </div>
-          <div class="col-md-4" style="margin-top:1em;">
-            <a href="check.php" class="btn btn-lg btn-yellow">Check your status<i class="fa fa-binoculars" aria-hidden="true" style="padding-left:0.6em;"></i></a>
-          </div>
-          <div class="col-md-3 col-md-offset-1" style="margin-top:1em;">
-              <a href="https://www.facebook.com/aparoksha/" target="_balnk" class="btn btn-lg btn-success">Event information<i class="fa fa-info-circle" aria-hidden="true" style="padding-left:0.6em;"></i></a>
+          <div class="col-md-6" style="margin-top:1em;">
+            <a href="#" class="btn btn-lg btn-yellow">Check your status<i class="fa fa-binoculars" aria-hidden="true" style="padding-left:0.6em;"></i></a>
           </div>
         </div>
 
         <div class="col-md-12 contact-form">
-            <h2 class="content-ct" style="font-weight:bold; margin-bottom:1em;"><span class="ti-email"></span> Check Your status here</h2>
+            <h2 class="content-ct" style="font-weight:bold; margin-bottom:1em;"><span class="ti-email"></span> Check Your order payment status</h2>
             
             <div class="alert alert-info col-sm-12" id="info" style="margin-bottom:1em; 
             <?php if(isset($_SESSION['confirm'])){echo("display:block;");} else {echo("display:none;");} ?> ">
               <?php if(isset($_SESSION['confirm'])){echo("{$_SESSION['confirm']}"); unset($_SESSION['confirm']);} ?>
             </div>
 
-            <form class="form-horizontal" data-toggle="validator" action="check-status.php" role="form" method="post" name="f">
+            <form class="form-horizontal" data-toggle="validator" role="form" method="get" name="f">
               <div class="form-group">
-                <label for="uemail" class="col-sm-2 control-label">Email<sup>*</sup></label>
+                <label for="order" class="col-sm-2 control-label">Order ID<sup>*</sup></label>
                 <div class="col-sm-9">
-                  <input type="email" class="form-control" id="uemail" name="uemail" placeholder="you@youremail.com" required>
+                  <input type="text" class="form-control" id="order" name="orderid" placeholder="Order ID as given in mail sent to you" required>
                   <div class="help-block with-errors pull-right"></div>
                   <span class="form-control-feedback" aria-hidden="true"></span>
                 </div>
               </div>
-              <div class="form-group">
-                  <label for="mobile" class="col-sm-2 control-label">Mobile<sup>*</sup></label>
-                  <div class="col-sm-9">
-                    <input type="number" class="form-control" id="mobile" name="mobile" placeholder="xxxxxxxxxx" required>
-                    <div class="help-block with-errors pull-right"></div>
-                    <span class="form-control-feedback" aria-hidden="true"></span>
-                  </div>
-              </div>
                 
               <div class="form-group">
                 <div class="col-sm-12" style="text-align:center; margin-top:1em;">
-                  <button type="submit" id="submit" name="sub1" class="btn btn-yellow">Submit<i class="fa fa-paper-plane" aria-hidden="true" style="padding-left:0.6em;"></i></button>
+                  <button type="submit" id="submit" class="btn btn-yellow">Submit<i class="fa fa-paper-plane" aria-hidden="true" style="padding-left:0.6em;"></i></button>
                 </div>
               </div>
             </form>
           </div>
         </div>
 
- <div class="container-fluid" style="<?php if(isset($_SESSION['name'])){echo("display:block;");} else {echo("display:none;");} ?>">
+ <div class="container-fluid" style="<?php if(isset($_GET['orderid']) && $order!=null ){echo("display:block;");} else {echo("display:none;");} ?>">
     <table class="table table-sm " style="width:80%; margin-left:auto; margin-right:auto;">
         <tbody>
             <tr>
                 <th scope="row" align="center">Name</th>
-                <td align="center"><?php if(isset($_SESSION['name'])){echo("{$_SESSION['name']}"); unset($_SESSION['name']);} ?></td>
+                <td align="center"><?php echo $name; ?></td>
             </tr>
             <tr>
-                <th scope="row" align="center">E-mail</th>
-                <td align="center"> <?php if(isset($_SESSION['email'])){echo("{$_SESSION['email']} "); unset($_SESSION['email']);}
-                  if(isset($_SESSION['verify'])){if($_SESSION['verify'] == "pending"){echo "(Unverified) <a href='resend.php'>resend mail </a>";}
-                  else{echo "(Verified)";} unset($_SESSION['verify']);}?></td>
+                <th scope="row" align="center">T-Shirt Type</th>
+                <td align="center"><?php echo $ttype; ?></td>
             </tr>
             <tr>
-                <th scope="row" align="center">College</th>
-                <td align="center"><?php if(isset($_SESSION['college'])){echo("{$_SESSION['college']}"); unset($_SESSION['college']);} ?></td>
+                <th scope="row" align="center">T-Shirt Size</th>
+                <td align="center"><?php echo $tsize; ?></td>
             </tr>
             <tr>
-                <th scope="row" align="center">Events Interested</th>
-                <td colspan="2" align="center"><?php if(isset($_SESSION['events'])){echo("{$_SESSION['events']}"); unset($_SESSION['events']);} ?> </td>
+                <th scope="row" align="center">T-Shirt Quantity</th>
+                <td align="center"><?php echo $quantity; ?></td>
+            </tr>
+            <tr>
+                <th scope="row" align="center">Transaction ID</th>
+                <td align="center"><?php echo $tranid; ?></td>
+            </tr>
+            <tr>
+                <th scope="row" align="center">Amount Paid</th>
+                <td align="center"><?php echo $amount; ?></td>
+            </tr>
+            <tr>
+                <th scope="row" align="center">Verified</th>
+                <td align="center"><?php if($verified === "true") {echo "Yes";} else {echo "No";} ?></td>
             </tr>
         </tbody>
     </table>
-  </div> -->
+  </div>
     
       <div class="container-fluid footer">
         <div class="col-sm-12" style="text-align:center; margin-top:1em; margin-bottom:0.5em;">
