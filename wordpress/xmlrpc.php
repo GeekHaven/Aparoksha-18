6a5064e84792a58611ad42d5968a776e4289cce3
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-03-06 03:29:42 +0530

    Revert confab to old page

---
+++ wordpress/xmlrpc.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>https://www.babylon-berlin.com/de</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://www.babylon-berlin.com/wordpress/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.babylon-berlin.com/wordpress/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.babylon-berlin.com/wordpress/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://www.babylon-berlin.com/wordpress/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.babylon-berlin.com/de/wp-json/" />
+	    </apis>
+  </service>
+</rsd>
